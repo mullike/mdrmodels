849a714e3bb0a939711711751e696687261f4d7a
Kenneth Mulligan <kenneth.mulligan@efsa.europa.eu>
2021-02-23 10:54:49 +0100

    sdfsddsf

--- AMRR4EU/dev/01_start.R
+++ AMRR4EU/dev/01_start.R
@@ -15,11 +15,11 @@
 ## Add meta data about your application
 golem::fill_desc(
   pkg_name = "AMRR4EU", # The Name of the package containing the App 
-  pkg_title = "PKG_TITLE", # The Title of the package containing the App 
-  pkg_description = "PKG_DESC.", # The Description of the package containing the App 
-  author_first_name = "AUTHOR_FIRST", # Your First Name
-  author_last_name = "AUTHOR_LAST", # Your Last Name
-  author_email = "AUTHOR@MAIL.COM", # Your Email
+  pkg_title = "AMR", # The Title of the package containing the App 
+  pkg_description = "Models for furhter analysis of Antimicrobial data", # The Description of the package containing the App 
+  author_first_name = "KENNETH", # Your First Name
+  author_last_name = "MULLIGAN", # Your Last Name
+  author_email = "KENNETH.MULLIGAN@EFSA.EUROPA.EU", # Your Email
   repo_url = NULL # The URL of the GitHub Repo (optional) 
 )     
 
